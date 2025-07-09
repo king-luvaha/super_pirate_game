dafa2adbc0afdfe5fe02e744935d1899fb684adb
Luvaha <luvaha456@gmail.com>
2025-07-10 00:54:32 +0300

    Removed a duplicate key, 'saw' in main.py

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ class Game:
             'small_chain': import_folder('.', 'assets', 'graphics', 'level', 'small_chains'),
             'candle_light': import_folder('.', 'assets', 'graphics', 'level', 'candle light'),
             'player': import_sub_folders('.', 'assets', 'graphics', 'player'),
-            'saw': import_folder('.', 'assets', 'graphics', 'enemies', 'saw', 'animation'),
             'saw_chain': import_image('.', 'assets', 'graphics', 'enemies', 'saw', 'saw_chain'),
             'helicopter': import_folder('.', 'assets', 'graphics', 'level', 'helicopter'),
             'boat': import_folder('.', 'assets', 'graphics', 'objects', 'boat'),
